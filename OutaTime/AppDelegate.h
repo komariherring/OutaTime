0de126c51fb3152c6ee787be176dbc9782a0fbd4
Komari Herring <komherr@gmail.com>
2016-07-20 13:38:42 -0400

    Initial Commit

---
+++ OutaTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OutaTime
+//
+//  Created by Komari Herring on 7/20/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OutaTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OutaTime
+//
+//  Created by Komari Herring on 7/20/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
